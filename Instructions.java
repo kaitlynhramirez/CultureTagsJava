945e7f12a99bf594bd3b72f9aa85b0d0288d3b4b
kaitlynhramirez <78514274+kaitlynhramirez@users.noreply.github.com>
2021-07-21 11:17:44 -0400

    Create Instructions.java

---
+++ Instructions.java
@@ -0,0 +1,87 @@
+package CultureTags;
+
+public class Instructions {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the game #CultureTags");
+		System.out.println("\n");
+		System.out.println("How to Play");
+		System.out.println("\n");
+		System.out.println("The objective of the game is to guess what the #CultureTag (acronym) stands for. There are multiple ways to play.");
+		System.out.println("\n");
+		System.out.println("#Teams");
+		System.out.println("\n");
+		System.out.println("1. Split into teams (pick your people wisely!) of two or more. The number of teams and the number of ");
+		System.out.println("   people beyond two is totally up to you, although we recommend no more than 4 to team.");
+		System.out.println("\n");
+		System.out.println("2. Choose a player on your team to be the describer (take turns each round).");
+		System.out.println("\n");
+		System.out.println("3. When the timer starts (90 secs), the describer grabs a stack of cards from the box and immediately:");
+		System.out.println("     # Announces the category and shows their team the #CultureTag");
+		System.out.println("     # Gives hints without saying any of the words in the phrase");
+		System.out.println("     # Puts the card down when it's answered correctly and continues flipping and giving hints");
+		System.out.println("     # Passes on the cards the team can't figure out by putting them in a separate pile");
+		System.out.println("\n");
+		System.out.println("4. When the timer runs out it's the next team's turn.");
+		System.out.println("     # Put the remaining cards that you didn't get through back in the box");
+		System.out.println("     # Leave the passed cards on the table");
+		System.out.println("     # Neatly stack the cards guessed correctly and put them off to the side");
+		System.out.println("     # Add one point for each card guessed correctly");
+		System.out.println("     # Deduct one point for each card passed");
+		System.out.println("\n");
+		System.out.println("5. Now it's the nex team's turn.");
+		System.out.println("     # The describer from the next team can decide to begin with the passed cards on the table OR grab a new stack");
+		System.out.println("       from the box");
+		System.out.println("     # The describer is not allowed to look through the passed stack again and the decision is either all or none");
+		System.out.println("     # If the describer decides to grab a new stack of cards from the box, continue playing as explained above");
+		System.out.println("     # If the describer begins with the passed cards, the objective and the play is the same, but the scoring is ");
+		System.out.println("       different");
+		System.out.println("     # If the describer gets through all of the passed cards before the timer runs out, grab a stack from the box ");
+		System.out.println("       and continue playing");
+		System.out.println("     # When the timer runs out, add two points for each passed card guessed correctly");
+		System.out.println("     # Deduct one point for each card passed (even if it was passed twice");
+		System.out.println("\n");
+		System.out.println("Once you've decided that you've played enough, and every team has had a chance, or if Martin re-runs start airing, ");
+		System.out.println("the team witht the most points at the end wins!");
+		System.out.println("\n");
+		System.out.println("#Every Man For Themselves");
+		System.out.println("\n");
+		System.out.println("1. Pick a describer and have them stand or sit where everyone can see them and the cards");
+		System.out.println("2. When the timer starts (90 secs), the describer grabs a stack of cards from the box and immediately:");
+		System.out.println("     # Announces the category and shows everyone the #CultureTag");
+		System.out.println("     # Gives hints without saying any of the words in the phrase");
+		System.out.println("     # Anyone playing can share the phrase oncce they know it");
+		System.out.println("     # When someone guesses the #CultureTag correctly, hand them the card");
+		System.out.println("3. When the timer runs out, it's another person's turn to be the describer");
+		System.out.println("4. Once everyone has had a chance to serve as the describer (or once Martin re-runs start airing), the person holding ");
+		System.out.println("   the most #CultureTags at the end wins");
+		System.out.println("\n");
+		System.out.println("#One On One");
+		System.out.println("\n");
+		System.out.println("The great thing about this game is you can play with a big team at a cookout or at a table with you and a friend. For");
+		System.out.println("one on one play, the set up is the same as teams, except it's just you and the other person. Whoever has the most points ");
+		System.out.println("at the end of your plays wins.");
+		System.out.println("\n");
+		System.out.println("#Rules For Hints");
+		System.out.println("\n");
+		System.out.println("You can provide detailed sentences, scenarios, sound effects, and gestures when you're describing the #CultureTag, but ");
+		System.out.println("you can't give the word or any variations of the word. You also can't say what it rhymes with or sounds like.");
+		System.out.println("\n");
+		System.out.println("For common words such as 'you' and 'the', those words are acceptable if they can't be avoided when providing the hint.");
+		System.out.println("\n");
+		System.out.println("For instance: YGMM? (You Got McDonald's Money?");
+		System.out.println("\n");
+		System.out.println("Acceptable hint: This is something your parents might say if you ask to go to a fast food restaurant");
+		System.out.println("\n");
+		System.out.println("Unacceptable hint: When I was younger, if I asked to go to McDonalds my parents would say, you...");
+		System.out.println("\n");
+		System.out.println("#House Rules");
+		System.out.println("\n");
+		System.out.println("We've included a few blamk cards to create your own #CultureTags. Use them for tie breakers or whenever you feel inspired. ");
+		System.out.println("The beauty of this game is your ability to add your unique spin. After all, it's your house and WYSG!");
+		System.out.println("\n");
+		System.out.println("#CultureTags");
+		System.out.println("A game for people who love hashtags | The Culture");
+    }
+}
